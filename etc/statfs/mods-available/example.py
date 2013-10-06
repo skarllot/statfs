205a698715cf417a66104054bd10a7cf9a0538ab
Fabrício Godoy <skarllot@gmail.com>
2013-10-06 03:33:21 -0300

    Example modules changed to fit planned features

--- etc/statfs/mods-available/example.py
+++ etc/statfs/mods-available/example.py
@@ -1,29 +1,63 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# example   Example module for statfs
-#
 # description:  Example module for statfs
-# param1:       Path for mounted statfs
-# param2:       Verbose mode (0=no | 1=yes)
 
+import argparse
 import os
 import sys
 from datetime import datetime
 
 MOD_NAME = "example"
-DEST = "%s/%s" % (sys.argv[1], MOD_NAME)
 
-VERBOSE = False
-if len(sys.argv) == 3 and sys.argv[2] == 1:
-    VERBOSE = True
+def update(args):
+    path = args.directory
+    if not(os.path.isdir(path)):
+        sys.stderr.write("Directory '%s' cannot be found\n" % path)
+        sys.exit(1)
 
-if not(os.path.isdir(DEST)):
-    if VERBOSE: print("Directory %s cannot be found, creating..." % DEST)
-    os.mkdir(DEST)
+    fname = "%s/update" % path
+    if args.verbose:
+        if not(os.path.isfile(fname)):
+            print("File '%s' does not exist, will be created" % fname)
 
-f = open("%s/update" % DEST, "w")
-f.write(str(datetime.now()) + "\n")
-f.close()
+    f = open(fname, "w")
+    f.write(str(datetime.now()) + "\n")
+    f.close()
+
+def show_frequency(args):
+    print("60")
+
+def show_dependencies(args):
+    print("")
+
+def show_name(args):
+    print(MOD_NAME)
+
+parser = argparse.ArgumentParser(description="Example module for statfs", formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=80))
+subparsers = parser.add_subparsers(title="Actions", metavar="")
+
+parser_f = subparsers.add_parser("default-frequency", \
+    help="gets default update frequency (in seconds)")
+parser_f.set_defaults(func=show_frequency)
+
+parser_d = subparsers.add_parser("dependencies", \
+    help="returns which modules this module depends")
+parser_d.set_defaults(func=show_dependencies)
+
+parser_n = subparsers.add_parser("name", \
+    help="returns this module name")
+parser_n.set_defaults(func=show_name)
+
+parser_u = subparsers.add_parser("update", \
+    help="updates info provided by this module")
+parser_u.add_argument("directory", type=os.path.abspath, \
+    help="path to mounted statfs")
+parser_u.add_argument("-v", "--verbose", action="store_true", \
+    help="verbose output")
+parser_u.set_defaults(func=update)
+
+args = parser.parse_args()
+args.func(args)
 
 # vim: set ts=4 et
--- etc/statfs/mods-available/example.sh
+++ etc/statfs/mods-available/example.sh
@@ -1,17 +1,136 @@
 #!/bin/bash
-# example   Example module for statfs
-#
 # description:  Example module for statfs
-# param1:       Path for mounted statfs
-# param2:       Verbose mode (0=no | 1=yes)
+#
 
 MOD_NAME=example
-DEST="$1/$MOD_NAME"
 
-if [ ! -d "$DEST" ]; then
-    [ $2 -eq 1 ] && echo "Directory $DEST cannot be found, creating..."
-    mkdir -p "$DEST"
+show_help() {
+    echo "Usage:
+    $MOD_NAME       update <directory> [-v]
+    $MOD_NAME       default-frequency
+    $MOD_NAME       dependencies
+    $MOD_NAME       name
+
+Actions:
+    default-frequency   gets default update frequency (in seconds)
+    dependencies        returns which modules this module depends
+    name                returns this module name
+    update              updates info provided by this module
+
+Options:
+    directory           path to mounted statfs
+    -h, --help          show this help message and exit
+    -v, --verbose       verbose output"
+}
+
+update() {
+    if [ ! -d "$1" ]; then
+        echo "Directory '$1' cannot be found" >&2
+        exit 1
+    fi
+
+    FNAME=$1/update
+    if [ $VERBOSE -eq 1 ]; then
+        if [ ! -f "$FNAME" ]; then
+            echo "File '$FNAME' does not exist, will be created"
+        fi
+    fi
+    date > "$FNAME"
+}
+
+show_frequency() {
+    echo "60"
+}
+
+show_dependencies() {
+    echo ""
+}
+
+show_name() {
+    echo "$MOD_NAME"
+}
+
+incompatible_args() {
+    echo "$MOD_NAME: You may not specify more than one '-uSD' option" >&2
+    echo "Refer to -h for more information" >&2
+    exit 2
+}
+
+if (($# == 0)); then
+    echo "$MOD_NAME: No arguments given (use -h for help)" >&2
+    exit 1
 fi
 
-date > "$DEST/update"
+getopt -T > /dev/null
+if [ $? -eq 4 ]; then
+    ARGS=$(getopt --name "$MOD_NAME" \
+        --long help,verbose \
+        --options hv -- "$@")
+else
+    ARGS=$(getopt hv "$@")
+fi
+if [ $? -ne 0 ]; then
+    echo "$MOD_NAME: usage error (use -h for help)" >&2
+    exit 2
+fi
+eval set -- $ARGS
 
+VERBOSE=0
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        -v|--verbose)
+            VERBOSE=1
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+    shift
+done
+
+if [ $# -eq 0 ]; then
+    echo "$MOD_NAME: You must specify one valid action" >&2
+    echo "Refer to -h for more information"
+    exit 2
+elif [ $# -eq 1 ]; then
+    case "$1" in
+        default-frequency)
+            show_frequency
+            ;;
+        dependencies)
+            show_dependencies
+            ;;
+        name)
+            show_name
+            ;;
+        update)
+            echo "$MOD_NAME: Missing 'directory' argument" >&2
+            echo "Refer to -h for more information"
+            exit 2
+            ;;
+        *)
+            echo "$MOD_NAME: You must specify one valid action" >&2
+            echo "Refer to -h for more information"
+            exit 2
+    esac
+elif [ $# -eq 2 ]; then
+    case "$1" in
+        update)
+            update $2
+            ;;
+        *)
+            echo "$MOD_NAME: You must specify one valid action" >&2
+            echo "Refer to -h for more information"
+            exit 2
+    esac
+else
+    echo "$MOD_NAME: Invalid arguments found (use -h for help)" >&2
+    exit 2
+fi
+
+# vim: set ts=4 et
